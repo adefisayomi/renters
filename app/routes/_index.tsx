9fa272b96ed2c5dc12efd486fc74f51dfce7cc9d
dolapo oluwole <adefisayomioluwole@gmail.com>
2024-08-11 16:47:33 +0100

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,40 +9,9 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="">
+      <h1 className="text-purple-500 text-5xl font-bold ">Hello there</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat vel, temporibus magnam facere et similique vitae molestias ipsum distinctio sapiente.</p>
     </div>
   );
 }
